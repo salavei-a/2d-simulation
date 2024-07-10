cfe4254d6793a483efae2a63d49befce4ee20a3f
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-07-10 20:41:59 +0400

    feat: add `size`, `getSize()` and `isSquareEmpty()` to `Map`

--- src/main/java/com/asalavei/model/common/Map.java
+++ src/main/java/com/asalavei/model/common/Map.java
@@ -1,9 +1,31 @@
 package com.asalavei.model.common;
 
 import com.asalavei.model.entities.Entity;
+import com.asalavei.model.entities.creatures.Predator;
 
 import java.util.HashMap;
 
 public class Map {
+    private int size;
     private HashMap<Coordinates, Entity> entities = new HashMap<>();
+
+    public Map(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isSquareEmpty(Coordinates coordinates) {
+        Coordinates coordinatesPredator1 = new Coordinates(1, 5);
+        Coordinates coordinatesPredator2 = new Coordinates(5, 2);
+        Coordinates coordinatesPredator3 = new Coordinates(7, 8);
+
+        entities.put(coordinatesPredator1, new Predator());
+        entities.put(coordinatesPredator2, new Predator());
+        entities.put(coordinatesPredator3, new Predator());
+
+        return !entities.containsKey(coordinates);
+    }
 }
