078f900fafc0dc4a79b7de2879e817daeb253632
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-07-10 20:39:08 +0400

    feat: add `row`, `column` fields and `equals()`, `hashCode()` to `Coordinates`

--- src/main/java/com/asalavei/model/common/Coordinates.java
+++ src/main/java/com/asalavei/model/common/Coordinates.java
@@ -1,4 +1,26 @@
 package com.asalavei.model.common;
 
+import java.util.Objects;
+
 public class Coordinates {
+    private final Integer row;
+    private final Integer column;
+
+    public Coordinates(Integer row, Integer column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return Objects.equals(row, that.row) && Objects.equals(column, that.column);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
+    }
 }
