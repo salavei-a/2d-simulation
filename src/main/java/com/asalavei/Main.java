d6d6735330ec2b386d672116c9c65c33e1587667
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-07-16 13:29:48 +0400

    feat: add `initActions()` to `Simulation`

--- src/main/java/com/asalavei/Main.java
+++ src/main/java/com/asalavei/Main.java
@@ -1,10 +1,8 @@
 package com.asalavei;
 
-import com.asalavei.model.common.Map;
-
 public class Main {
     public static void main(String[] args) {
-        Simulation simulation = new Simulation(new Map(10));
+        Simulation simulation = new Simulation(new MapFactory().createMap(10));
 
         simulation.start();
     }
--- src/main/java/com/asalavei/Simulation.java
+++ src/main/java/com/asalavei/Simulation.java
@@ -1,28 +1,33 @@
 package com.asalavei;
 
 import com.asalavei.actions.Action;
-import com.asalavei.model.common.Map;
+import com.asalavei.actions.InitEntity;
+import com.asalavei.model.common.WorldMap;
 import com.asalavei.view.MapConsoleRenderer;
 import com.asalavei.view.Renderer;
 
 public class Simulation {
-    private Map map;
+    private WorldMap map;
     private int turnCounter;
     private Action action;
     private final Renderer renderer = new MapConsoleRenderer();
 
-    public Simulation(Map map) {
+    public Simulation(WorldMap map) {
         this.map = map;
     }
 
     public void start() {
         int end = 0;
+        map = initActions(map);
 
-        while (end != 1 ) {
+        while (end != 1) {
             renderer.render(map);
-
             end++;
         }
+    }
+
+    private WorldMap initActions(WorldMap map) {
+        return new InitEntity().doAction(map);
     }
 
     public void nextTurn() {
