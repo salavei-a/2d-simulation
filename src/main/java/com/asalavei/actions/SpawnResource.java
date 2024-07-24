6ccca8f033fd34041654d7f54c7263ec20c3d7d1
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-07-24 16:18:50 +0400

    feat: add spawn rate control to `SpawnResource`

--- src/main/java/com/asalavei/actions/SpawnResource.java
+++ src/main/java/com/asalavei/actions/SpawnResource.java
@@ -5,17 +5,38 @@ import com.asalavei.EntityFactory;
 import com.asalavei.model.common.Coordinates;
 import com.asalavei.model.common.WorldMap;
 import com.asalavei.model.entities.Entities;
+import com.asalavei.model.entities.Entity;
+import com.asalavei.model.entities.environment.Resource;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class SpawnResource extends Action {
+    private static final double SPAWN_RATE = 0.2;
 
     @Override
     public WorldMap doAction(WorldMap map) {
-        Coordinates coordinates = CoordinatesFactory.getRandomCoordinates(map.getSize());
+        Set<Entity> resources = map.getEntities().values()
+                .stream()
+                .filter(Resource.class::isInstance)
+                .collect(Collectors.toSet());
 
-        if (map.isPlaceEmpty(coordinates)) {
-            map.setEntity(coordinates, new EntityFactory().createEntity(Entities.GRASS, coordinates));
+        double currentRate = resources.size() / Math.pow(map.getSize(), 2);
+
+        if (currentRate < SPAWN_RATE) {
+            spawnResource(map);
         }
 
         return map;
+    }
+
+    private static void spawnResource(WorldMap map) {
+        Coordinates coordinates;
+
+        do {
+            coordinates = CoordinatesFactory.getRandomCoordinates(map.getSize());
+        } while (!map.isPlaceEmpty(coordinates));
+
+        map.setEntity(coordinates, new EntityFactory().createEntity(Entities.GRASS, coordinates));
     }
 }
