455e2da50f6f59dc755d704cd0a652a3bbfc5b8b
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-07-28 14:11:07 +0400

    refactor: improve `PathFinder` logic and replace hardcoded adjacency radius with variable

--- src/main/java/com/asalavei/PathFinder.java
+++ src/main/java/com/asalavei/PathFinder.java
@@ -16,10 +16,10 @@ public class PathFinder {
     }
 
     public static Coordinates getPlaceToMove(Creature creature, WorldMap map) {
-        Map<Coordinates, Coordinates> cameFrom = new HashMap<>();
         Queue<Coordinates> queue = new LinkedList<>();
         Set<Coordinates> explored = new HashSet<>();
-        List<Coordinates> movementRange = getAvailableMovementCoordinates(creature, map);
+        Map<Coordinates, Coordinates> cameFrom = new HashMap<>();
+        List<Coordinates> availableMovementCoordinates = getAvailableMovementCoordinates(creature, map);
 
         Coordinates start = creature.getCoordinates();
         queue.add(start);
@@ -29,16 +29,16 @@ public class PathFinder {
             Coordinates current = queue.poll();
 
             if (isTarget(creature, map.getEntity(current))) {
-                return findPathToTarget(start, current, cameFrom, movementRange, map);
+                return findPathToTarget(start, current, cameFrom, availableMovementCoordinates);
             }
 
-            for (Coordinates neighbor : getAdjacentCoordinates(current, map)) {
-                if (!explored.contains(neighbor) && !cameFrom.containsKey(neighbor)) {
-                    if (map.isPlaceEmpty(neighbor) || isTarget(creature, map.getEntity(neighbor))) {
+            for (Coordinates neighbor : getAdjacentCoordinates(current, map, 1)) {
+                if (!explored.contains(neighbor) &&
+                    !cameFrom.containsKey(neighbor) &&
+                    (map.isPlaceEmpty(neighbor) || isTarget(creature, map.getEntity(neighbor)))) {
                         queue.add(neighbor);
                         cameFrom.put(neighbor, current);
                         explored.add(neighbor);
-                    }
                 }
             }
         }
@@ -47,50 +47,36 @@ public class PathFinder {
     }
 
     private static List<Coordinates> getAvailableMovementCoordinates(Creature creature, WorldMap map) {
-        int baseRow = creature.getCoordinates().getRow();
-        int baseColumn = creature.getCoordinates().getColumn();
-        int speed = creature.getSpeed();
+        List<Coordinates> availableMovementCoordinates = new ArrayList<>();
 
-        List<Coordinates> availableCoordinates = new ArrayList<>();
-
-        for (int i = -speed; i <= speed; i++) {
-            for (int j = -speed; j <= speed; j++) {
-                if (i == 0 && j == 0) continue;
-
-                int newRow = baseRow + i;
-                int newColumn = baseColumn + j;
-
-                if (map.isWithinMapBounds(newRow, newColumn)) {
-                    availableCoordinates.add(CoordinatesFactory.createCoordinates(newRow, newColumn));
-                }
+        for (Coordinates coordinates : getAdjacentCoordinates(creature.getCoordinates(), map, creature.getSpeed())) {
+            if (map.isPlaceEmpty(coordinates)) {
+                availableMovementCoordinates.add(coordinates);
             }
         }
-
-        Collections.shuffle(availableCoordinates);
-
-        return availableCoordinates;
+        return availableMovementCoordinates;
     }
 
-    private static List<Coordinates> getAdjacentCoordinates(Coordinates coordinates, WorldMap map) {
+    private static List<Coordinates> getAdjacentCoordinates(Coordinates coordinates, WorldMap map, int adjacencyRadius) {
         int baseRow = coordinates.getRow();
         int baseColumn = coordinates.getColumn();
 
-        List<Coordinates> availableCoordinates = new ArrayList<>();
+        List<Coordinates> adjacentCoordinates = new ArrayList<>();
 
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
+        for (int i = -adjacencyRadius; i <= adjacencyRadius; i++) {
+            for (int j = -adjacencyRadius; j <= adjacencyRadius; j++) {
                 if (i == 0 && j == 0) continue;
 
                 int newRow = baseRow + i;
                 int newColumn = baseColumn + j;
 
                 if (map.isWithinMapBounds(newRow, newColumn)) {
-                    availableCoordinates.add(CoordinatesFactory.createCoordinates(newRow, newColumn));
+                    adjacentCoordinates.add(CoordinatesFactory.createCoordinates(newRow, newColumn));
                 }
             }
         }
 
-        return availableCoordinates;
+        return adjacentCoordinates;
     }
 
     private static boolean isTarget(Creature creature, Entity entity) {
@@ -98,7 +84,7 @@ public class PathFinder {
                 creature instanceof Herbivore && entity instanceof Resource;
     }
 
-    private static Coordinates findPathToTarget(Coordinates start, Coordinates goal, Map<Coordinates, Coordinates> cameFrom, List<Coordinates> movementRange, WorldMap map) {
+    private static Coordinates findPathToTarget(Coordinates start, Coordinates goal, Map<Coordinates, Coordinates> cameFrom, List<Coordinates> availableMovementCoordinates) {
         LinkedList<Coordinates> path = new LinkedList<>();
         Coordinates current = goal;
 
@@ -108,7 +94,7 @@ public class PathFinder {
         }
 
         for (Coordinates step : path) {
-            if (movementRange.contains(step) && map.isPlaceEmpty(step)) {
+            if (availableMovementCoordinates.contains(step)) {
                 return step;
             }
         }
