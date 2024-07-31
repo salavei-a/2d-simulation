378efd76ba9ab320f2a2ea1b5252be31b76426e3
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-07-31 16:31:35 +0400

    feat: `SimulationController` extracted

--- src/main/java/com/asalavei/simulation/Main.java
+++ src/main/java/com/asalavei/simulation/Main.java
@@ -1,45 +1,19 @@
 package com.asalavei.simulation;
 
-import com.asalavei.simulation.factory.WorldMapFactory;
-
-import java.util.Optional;
 import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
-        Renderer renderer = new ConsoleRenderer();
+        SimulationController simulationController = new SimulationController();
         Scanner scanner = new Scanner(System.in);
-        boolean isRunning = true;
 
-        Optional<Simulation> simulation = Optional.empty();
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        simulationController.getRenderer().printControls();
 
-        renderer.printControls();
-
-        while (isRunning) {
-            String input = scanner.nextLine();
-            Command command = Command.fromString(input);
-
-            switch (command) {
-                case START_NEW_SIMULATION -> {
-                    simulation.ifPresent(Simulation::stop);
-
-                    simulation = Optional.of(new Simulation(WorldMapFactory.createMap(10)));
-                    executorService.submit(simulation.get()::start);
-                }
-                case PAUSE_RESUME ->
-                        simulation.ifPresentOrElse(Simulation::togglePause, renderer::printActionUnavailable);
-                case QUIT -> {
-                    simulation.ifPresent(Simulation::stop);
-                    isRunning = false;
-                }
-                default -> renderer.printInvalidCommand(input);
-            }
+        while (simulationController.isRunning()) {
+            simulationController.handleCommand(scanner.nextLine());
         }
 
-        executorService.shutdown();
+        simulationController.shutdown();
         scanner.close();
     }
 }
---
+++ src/main/java/com/asalavei/simulation/SimulationController.java
@@ -0,0 +1,51 @@
+package com.asalavei.simulation;
+
+import com.asalavei.simulation.factory.WorldMapFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SimulationController {
+    private final Renderer renderer = new ConsoleRenderer();
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private Simulation simulation = null;
+    private boolean isRunning = true;
+
+    public void handleCommand(String input) {
+        Command command = Command.fromString(input);
+
+        switch (command) {
+            case START_NEW_SIMULATION -> {
+                if (simulation != null) simulation.stop();
+
+                simulation = new Simulation(WorldMapFactory.createMap(10));
+                executorService.submit(simulation::start);
+            }
+            case PAUSE_RESUME -> {
+                if (simulation != null) {
+                    simulation.togglePause();
+                } else {
+                    renderer.printActionUnavailable();
+                }
+            }
+            case QUIT -> {
+                if (simulation != null) simulation.stop();
+
+                isRunning = false;
+            }
+            default -> renderer.printInvalidCommand(input);
+        }
+    }
+
+    public Renderer getRenderer() {
+        return renderer;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public void shutdown() {
+        executorService.shutdown();
+    }
+}
